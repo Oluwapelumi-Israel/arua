fc6ae92a54e9ee0d2bedf0984b7fc59facf42ebf
Oluwapelumi-Israel <israeloluwapelumi@gmail.com>
2023-10-13 00:48:04 +0100

    Added place holders

---
+++ JavaScript/Help/config.js
@@ -0,0 +1,61 @@
+
+
+
+let speakers = {
+    keynotespeakers: {
+        name: [
+            'Professor David Mills',
+
+            'Professor Evelyn Welch',
+
+            'Professor Adam Habib',
+
+            'Professor Peter Maassen',
+
+            'Professor Kwame Akyeampong'
+        ],
+        title: [
+            'Vice-President of Kellogg College & Director of the Centre for Global Higher Education, University of Oxford',
+
+            'Vice-Chancellor & President, University of Bristol',
+
+            'Director, School of Oriental and African Studies (SOAS),  University of London',
+
+            'Deputy Head/Research Coordinator, Department for Educational ResearchFaculty of Education, University of Oslo',
+
+            'Director of the Centre for the Study of Global Development (CSGD), Open University, UK',
+        ],
+        profile: [
+            '<p>David Mills is an Associate Professor in the Department of Education at the University of Oxford, Vice-President of Kellogg College, and Deputy Director of the Centre for Global Higher Education. </p><p>His research interests include the postcolonial history of universities in Anglophone Africa, doctoral education, and the political economy of the global science communication system. His most recent book is ‘Who Counts: Ghanaian Academic Publishing and Global Science’, co-written with colleagues from the University of Ghana and from Oxford, and available open access from African Minds:  <a href="https://www.africanminds.co.za/who-counts/" target="_blank">https://www.africanminds.co.za/who-counts/</a></p>',
+
+            '<p>Evelyn became Vice-Chancellor and President of the University of Bristol in September 2022.</p><p>Evelyn Welch graduated from Harvard University, receiving her PhD from the Warburg Institute, University of London. She was previously Senior Vice-President for Service, People & Planning at King’s College London, and had been Vice-President (Arts and Sciences) and Provost (Arts and Sciences). She has taught at University of Essex, the Warburg Institute, and held leadership roles at University of Sussex (PVC Teaching and Learning) and Queen Mary, University of London (Dean, Arts and PVC Research and International). As Professor of Renaissance Studies, she has led major research programmes including ‘The Material Renaissance,’ and ‘Beyond Text: Performances, Sounds, Images’. She recently completed a Wellcome Trust Senior Investigator Award for a project on ‘Renaissance Skin’ and has authored numerous books, including Fashioning the Early Modern: Creativity and Innovation in Europe, 1500-1800 (OUP 2017), and Shopping in the Renaissance (Yale 2005), winning the Wolfson Prize for History.</p>',
+
+            '<p>Adam Habib is an academic, researcher, activist, administrator, and well-known public intellectual. A Professor of Political Science, Habib has over 30 years of academic, research and administration expertise, spanning five universities and multiple local and international institutions.</p><p>Prior to his appointment as Director of SOAS, he was Vice-Chancellor and Principal of the University of the Witwatersrand (Wits) in Johannesburg, South Africa between 2013 and 2020. He has also served as Deputy Vice-Chancellor of Research at the University of Johannesburg, Executive Director of Democracy & Governance at the Human Science Research Council and director of the Centre for Civil Society and Professor of Development at the University of KwaZulu-Natal. He is widely published, among which are his two well received monographs, South Africa\'s Suspended Revolution: Hopes and Prospects and Rebel & Rage: Reflecting on #FeesMustFall.</p><p>Habib’s academic contributions resulted in his election to the American Academy of Arts and Sciences, in addition to serving as a fellow of both the African Academy of Science and the Academy of Science of South Africa. He also serves on the Council of the United Nations University.</p>',
+
+            '<p>Peter Maassen is professor in Higher Education and Science studies at the University of Oslo, Norway, and extraordinary professor at Stellenbosch University, South Africa. His main research interests are public governance and policy, academic freedom, and organizational development in higher education and science.</p><p>He has participated in many national and international research projects, expert committees, and evaluation panels. He has taken a number of leadership positions in higher education and is currently a member of the executive board of the Barratt Due Music Academy, Oslo, and a special envoy of the Guild for Africa. He has produced over 250 international publications.</p>',
+
+            '<p>Kwame Akyeampong is Professor of International Education and Development and Director of the Centre for the Study of Global Development (CSGD) at the Open University, UK and was previously a professor of international education and development at the University of Sussex, UK for 18 years.</p><p>He served as a senior policy analyst with UNESCO’s Global Monitoring Report team in Paris from 2011 to 2013.  He has researched and published on teacher education and led large scale impact evaluation studies on education interventions in several African countries. Kwame co-chairs the World Bank and the UK Foreign, Commonwealth and Development Office (FCDO) Global Education Evidence Advisory Panel (GEEAP).</p>',
+        ],
+
+        category: [
+            'Final Keynote Speaker',
+
+            'Second Keynote Speaker',
+
+            'First Keynote Speaker ',
+
+            'Third Keynote Speaker',
+
+            'Fourth Keynote Speaker',
+        ]
+    },
+    conferencespeakers: {
+
+    },
+    sessionschairs: {
+
+    },    
+}
+
+
+export default speakers
---
+++ index.js
@@ -0,0 +1,154 @@
+import speakers from "./JavaScript/Help/config.js";
+// Modals JavaScript Variables
+let availableSpeakers = document.querySelectorAll('.speaks.available');
+let availableSpeakersImage = document.querySelectorAll('.speaks.available .img img');
+let modalImage = document.querySelector('.modal_container .modal_img img');
+let name = document.querySelector('.modal_container .modal_description .modal_name');
+let title = document.querySelector('.modal_container .modal_description .modal_title');
+let category = document.querySelector('.modal_container .modal_description .modal_category');
+let profile = document.querySelector('.modal_container .modal_description .profile');
+let cancel = document.querySelector('.modal .cancel');
+let modal = document.querySelector('.modal')
+
+
+for(let i = 0; i < availableSpeakers.length; i++)
+{
+    availableSpeakers[i].addEventListener('click' , () => {
+        modalImage.src = availableSpeakersImage[i].src;
+        name.innerText = speakers.keynotespeakers.name[i];
+        title.innerText = speakers.keynotespeakers.title[i];
+        category.innerText = speakers.keynotespeakers.category[i];
+        profile.innerHTML = speakers.keynotespeakers.profile[i];
+
+        modal.className = "modal display";
+
+    });
+}
+
+// Programme Days JavaScript Manipulation
+
+let dayone = document.querySelector('.day_1');
+let daytwo = document.querySelector('.day_2');
+let daythree = document.querySelector('.day_3');
+
+let navdayone = document.querySelector('.days section:nth-of-type(1)');
+let navdaytwo = document.querySelector('.days section:nth-of-type(2)');
+let navdaythree = document.querySelector('.days section:nth-of-type(3)');
+
+
+
+// time JavaScript Manipulation
+
+
+let daysd = document.getElementById('days');
+let days = document.querySelector('.days p:nth-of-type(1)');
+let hoursd = document.getElementById('hours');
+let hours = document.querySelector('.hours p:nth-of-type(1)');
+let minutesd = document.getElementById('minutes');
+let minutes = document.querySelector('.minutes p:nth-of-type(1)');
+let secondsd = document.getElementById('seconds');
+let seconds = document.querySelector('.seconds p:nth-of-type(1)');
+
+let dd = document.getElementById('dd');
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+
+let day_dot = document.querySelector('.day_dot');
+let hr_dot = document.querySelector('.hr_dot');
+let min_dot = document.querySelector('.min_dot');
+let sec_dot = document.querySelector('.sec_dot');
+
+let endDate = '11/15/2023 00:00:00';
+            // date format mm/dd/yy
+
+let x = setInterval(()=>{
+
+    let now = new Date(endDate).getTime();
+    let countDown = new Date().getTime();
+    let distance = now - countDown;
+
+    // time calculation for days, hours, minutes, seconds
+    let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((distance % (1000 * 60)) / (1000));
+
+    // output the result in the element with id
+    daysd.innerHTML = `${d} <br><span style="margin-top: 10px; font-size: 0.5em; font-weight: bold;">days<span>`;
+    days.innerHTML = `${d}`;
+    hoursd.innerHTML = `${h} <br><span style="margin-top: 10px;font-size: 0.5em;font-weight: bold;">hours<span>`;
+    hours.innerHTML = `${h}`;
+    minutesd.innerHTML = `${m} <br><span style="margin-top: 10px; font-size: 0.5em;font-weight: bold;">minutes<span>`;
+    minutes.innerHTML = `${m}`;
+    secondsd.innerHTML = `${s} <br><span style="margin-top: 10px; font-size: 0.5em;font-weight: bold;">seconds<span>`;
+    seconds.innerHTML = `${s}`;
+
+    // animate stroke
+    dd.style.strokeDashoffset = 440 - (400 * d) / 365;
+    // 365 days in a year
+    hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+    // 24 hrs in a day
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    // 60 minutes in an hour
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+    // 60 seconds in a minute
+
+
+    if(distance < 0)
+    {
+        clearInterval(x);
+        document.getElementById("time_1").style.display = 'none';
+        document.querySelector('.time_2').style.display = 'none'
+        document.querySelector('.newyear').style.display = 'block';
+    }
+},);
+
+
+navdayone.addEventListener('click' , () => {
+    removeVisibility();
+    removeActive();
+    dayone.classList.remove('visible');
+    navdayone.classList.add('active');
+});
+
+navdaytwo.addEventListener('click' , () => {
+    removeVisibility();
+    removeActive();
+    daytwo.classList.remove('visible');
+    navdaytwo.classList.add('active');
+});
+
+navdaythree.addEventListener('click' , () => {
+    removeVisibility();
+    removeActive();
+    daythree.classList.remove('visible');
+    navdaythree.classList.add('active');
+});
+
+
+
+let removeVisibility = () => {
+    dayone.className = "day_1 visible";
+    daytwo.className = "day_2 visible";
+    daythree.className = "day_3 visible";
+};
+
+let removeActive = () => {
+    navdayone.className = '';
+    navdaytwo.className = '';
+    navdaythree.className = '';
+};
+
+
+
+// Speakers Section JavaScript...
+cancel.addEventListener('click' , ()=>{
+    modal.className = "modal";
+});
+
+
+
+
+
+
